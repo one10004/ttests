52caf49a2e6d4ef516aa6467e793bad346934c6e
one10004 <ryunhoo123@kookmin.ac.kr>
2018-03-26 13:33:22 +0900

    dsds

--- gitwatcher/views.py
+++ gitwatcher/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+import zipfile
 # Create your views here.
 
 def index(request):
 	return render(request,'gitwatcher/index.html')
 
 
-def gitwatch(request, address):
+def gitwatch1(request, address):
 	address = 'https://github.com/' + address
 	os.chdir('/home/ubuntu')
 	os.system('git clone ' + address + '.git')
 	words = address.split('/')
 	length = len(words)
 	os.chdir('/home/ubuntu/' + words[length-1])
-	os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/ttests/gitwatcher/templates/gitwatcher/statistics.html')
-	return render(request, 'gitwatcher/statistics.html')
+	os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/ttests/gitwatcher/templates/gitwatcher/statistics1.html')
+	return render(request, 'gitwatcher/statistics1.html')
+
+
+def gitwatch2(request, address):
+	words = address.split('/')
+	length = len(words)
+	address = words[length-1]
+	os.chdir('/home/ubuntu')
+	os.system('aws s3 cp s3://ec2s3example/' + address + '.zip ./')
+	os.system('unzip ' + address +'.zip')
+	os.chdir('/home/ubuntu/' + address + '-master')
+	os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/ttests/gitwatcher/templates/gitwatcher/statistics2.html')
+	return render(request, 'gitwatcher/statistics2.html')

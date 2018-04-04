1a20dc825475225a5013bac101d4ae39cff9eb76
one10004 <ryunhoo123@kookmin.ac.kr>
2018-04-04 15:36:13 +0900

    sa

--- gitwatcher/views.py
+++ gitwatcher/views.py
@@ -22,13 +22,24 @@ def gitwatch1(request, address):
 
 
 def gitwatch2(request, address):
-	words = address.split('/')
-	length = len(words)
-	address = words[length-1]
-	os.chdir('/home/ubuntu')
-	os.system('aws s3 cp s3://ec2s3example/' + address + '.zip ./')
-	os.system('unzip ' + address +'.zip -d ./' + address)
-	os.chdir('/home/ubuntu/' + address)
-	git.main()
-	#os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/ttests/gitwatcher/templates/gitwatcher/statistics2.html')
-	return render(request, 'gitwatcher/statistics2.html')
+   words = address.split('/')
+   length = len(words)
+   address = words[length-1]
+
+   BUCKET_NAME = 'gittos3fixed-outputbucket-ix0jgogj97ai'
+   KEY = 'kookmin-sw/2018-cap1-11/branch/master/kookmin-sw_2018-cap1-11_branch_master.zip'
+   s3 = boto3.resource('s3')
+   try:
+       s3.Bucket(BUCKET_NAME).download_file(KEY, '\\home\\ubuntu\\다운로드성공.zip')
+   except botocore.exceptions.Clienterror as e:
+       if e.response['Error']['Code'] == '404':
+           print("The object does not exist.")
+       else:
+           raise
+
+   os.chdir('/home/ubuntu')
+   os.system('unzip 다운로드성공.zip -d ./다운로드성공')
+   os.chdir('/home/ubuntu/다운로드성공')
+   git.main()
+   #os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/ttests/gitwatcher/templates/gitwatcher/statistics2.html')
+   return render(request, 'gitwatcher/statistics2.html')

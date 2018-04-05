dfacd29bc2cd082166b2991eecabb29b05fade7e
one10004 <ryunhoo123@kookmin.ac.kr>
2018-04-05 12:31:34 +0900

    sds

--- gitwatcher/views.py
+++ gitwatcher/views.py
@@ -47,5 +47,8 @@ def gitwatch2(request, address, branch):
 	os.system('unzip git_repo.zip -d ./git_repo')
 	os.chdir('/home/ubuntu/git_repo')
 	git.main()
+	os.chdir('/home/ubuntu')
+	os.system('rm -rf git_repo.zip')
+	os.system('rm -rf git_repo')
 	#os.system('python3 /home/ubuntu/gitinspector/gitinspector.py -F html > /home/ubuntu/ttests/gitwatcher/templates/gitwatcher/statistics2.html')
 	return render(request, 'gitwatcher/statistics2.html')

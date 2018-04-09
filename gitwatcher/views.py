241debe376a8db0ebef614a55facc66a914aae9e
one10004 <ryunhoo123@kookmin.ac.kr>
2018-04-09 16:42:52 +0900

    saa

--- gitwatcher/views.py
+++ gitwatcher/views.py
@@ -33,6 +33,7 @@ def storedData(request):
 	path = '/home/ubuntu/html'
 	files = os.listdir(path)
 	context = RequestContext(request,{'params_list':{'name':'jee'}})
+	context = {'name':'jee'}
 	return render(request,'gitwatcher/storedData.html',context)
 
 def gitwatch2(request, address, branch):
